6a9ab5cae9fdf9329f15ef3342a3d44b52c99b3f
daniestcordova18 <daniestcordova18@gmail.com>
2024-11-13 17:00:23 +0000

    first commit

---
+++ src/app/components/buttons/accept-button/accept-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './accept-button.component.html',
+  styleUrl: './accept-button.component.css'
+})
+export class AcceptButtonComponent {
+
+}
---
+++ src/app/components/buttons/dark-mode-button/dark-mode-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './dark-mode-button.component.html',
+  styleUrl: './dark-mode-button.component.css'
+})
+export class DarkModeButtonComponent {
+
+}
